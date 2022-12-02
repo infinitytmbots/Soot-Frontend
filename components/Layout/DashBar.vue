852d0f212526cac9fb25f74767baada01fc3c01a
Joshua V <69136448+infinitytmbots@users.noreply.github.com>
2022-12-03 04:18:14 +0530

    Update DashBar.vue

--- components/Layout/DashBar.vue
+++ components/Layout/DashBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-full w-auto pt-16  ml-auto pr-4 bg-white-primary">
+  <div class="flex h-screen overflow-y-scroll w-auto pt-16  ml-auto pr-8 pl-4 bg-white-primary">
             <div class="grid justify-items-center space-evenly">
                 <img src="/img/Frame 10.png" alt="">
                 <img src="/img/Frame 9.png" alt="">
