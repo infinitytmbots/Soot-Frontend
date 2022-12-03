1bda83de8e3022cf4b4a8b6598b85a6d62fd8865
Glitchyi <advaithnarayanan8@gmail.com>
2022-12-03 07:33:55 +0530

    change

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,4 +1,14 @@
 <template>
+    These Terms and Conditions have been created to ensure a positive experience for all
+parties using the Project.By selling carbon credits on the Project
+Marketplace you accept these Seller terms and conditions (the “Terms and
+Conditions”).
+
+
+To accept and adopt these Terms and Conditions, please click on the ‘I Accept’ box in
+the Submission Form. Upon submission of your project
+information and acceptance of these Terms and Conditions, Soot will review
+your submission.
 </template>
 <script setup>
     definePageMeta({
